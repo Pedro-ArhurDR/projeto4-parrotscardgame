28828e20a4883d0aaa30a8771c47fbf3506efd3c
Pedro Arthur <pedroarthurdr@hotmail.com>
2022-06-19 15:47:34 -0300

    animação carta unica

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function virarcarta(){
-    alert("aa")
+   const elemento = document.querySelector(".carta")
+   elemento.classList.toggle("flip")
+   const elemento2 = document.querySelector(".front")
+   elemento2.classList.toggle("show")
 }
